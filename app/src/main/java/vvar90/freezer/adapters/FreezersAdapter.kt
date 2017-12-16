2a39f5ea8cfd722deaf891986821db35875ec34b
vvar90 <vvar90@gmail.com>
2017-12-16 18:48:35 +0100

    Change ListView for RecyclerView in FreezersActivity

--- app/src/main/java/vvar90/freezer/adapters/FreezersAdapter.kt
+++ app/src/main/java/vvar90/freezer/adapters/FreezersAdapter.kt
@@ -1,51 +1,42 @@
 package vvar90.freezer.adapters
 
-import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.Adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
-import vvar90.freezer.database.DatabaseHandler
 import vvar90.freezer.R
+import vvar90.freezer.dao.Freezer
 
 /**
  * Created by vvar9 on 08/12/2017.
  */
-class FreezersAdapter(context: Context, freezers: List<String>) : BaseAdapter() {
+class FreezersAdapter(freezers: List<Freezer>) : Adapter<FreezersAdapter.FreezersViewHolder>() {
 
-    private val mInflator: LayoutInflater = LayoutInflater.from(context)
-    private var freezerList: List<String> = freezers
+    private var freezerList: List<Freezer> = freezers
 
-    override fun getItem(position: Int): Any {
-        return freezerList[position]
+    class FreezersViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+        var deleteButton: ImageButton = itemView?.findViewById(R.id.deleteFreezerBtn)!!
+        var freezerName: TextView = itemView?.findViewById(R.id.itemText)!!
+        var snowFlakePic: ImageView = itemView?.findViewById(R.id.snowFlakeImage)!!
     }
 
-    override fun getItemId(position: Int): Long {
-        return position.toLong()
+    override fun onBindViewHolder(holder: FreezersViewHolder, position: Int) {
+        var freezer = freezerList[position]
+        holder.freezerName.text = freezer.name
     }
 
-    override fun getCount(): Int {
+    override fun getItemCount(): Int {
         return freezerList.size
     }
 
-    override fun getView(position1: Int, convertView: View?, parent: ViewGroup?): View? {
-        val view: View?
-        val vh: ListRowHolder
-        if (convertView == null) {
-            view = this.mInflator.inflate(R.layout.list_row, parent, false)
-            vh = ListRowHolder(view)
-            view?.tag = vh
-        } else {
-            view = convertView
-            vh = view.tag as ListRowHolder
-        }
-
-        vh.label.text = freezerList[position1].toString()
-        return view
-    }
-    private class ListRowHolder(row: View?) {
-        val label: TextView = row?.findViewById<TextView>(R.id.label) as TextView
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): FreezersViewHolder {
+        val itemView = LayoutInflater.from(parent?.context).inflate(R.layout.list_row,
+                parent, false)
+        return FreezersViewHolder(itemView)
     }
 }
 
--- app/src/main/java/vvar90/freezer/dao/Freezer.kt
+++ app/src/main/java/vvar90/freezer/dao/Freezer.kt
@@ -3,4 +3,4 @@ package vvar90.freezer.dao
 /**
  * Created by vvar9 on 07/12/2017.
  */
-data class Freezer(var name: String)
+data class Freezer(val name: String)
